8e48645fd86d883b058d8f8a7591ebe6c4e6ada3
Krisantus Bahaso <krisantus@bahaso.com>
2017-06-13 11:17:24 +0700

    production firebase project mode on

--- index.v114.js
+++ index.v114.js
@@ -1,11 +1,11 @@
 $(function () {
     var config = {
-        apiKey: "AIzaSyBy6ff_NDaYxwRK5M6rlvojzmZnfBzGfCI",
-        authDomain: "fir-bahaso.firebaseapp.com",
-        databaseURL: "https://fir-bahaso.firebaseio.com",
-        projectId: "fir-bahaso",
-        storageBucket: "fir-bahaso.appspot.com",
-        messagingSenderId: "134825814680"
+        apiKey: "AIzaSyBd-we3DS2sZBhExI-VdJrNyKypR_qFAEI",
+        authDomain: "bahaso-sketch.firebaseapp.com",
+        databaseURL: "https://bahaso-sketch.firebaseio.com",
+        projectId: "bahaso-sketch",
+        storageBucket: "bahaso-sketch.appspot.com",
+        messagingSenderId: "267090960672"
     };
 
     firebase.initializeApp(config);
