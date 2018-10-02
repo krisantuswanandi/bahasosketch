b2925ebbfa9c6453a7b4994a43fc58740a832685
krisantusbahaso <krisantus@bahaso.com>
2018-10-02 19:57:39 +0700

    build new app using react-scripts

---
+++ docs/precache-manifest.35f932c961e63a3693564e3fdd64a929.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2adec7ffbab99c36e1bb6ed2ce69aab",
+    "url": "/static/media/inktoberv.b2adec7f.png"
+  },
+  {
+    "revision": "47404842579953936427643d1bc6e2f6",
+    "url": "/static/media/inktoberh.47404842.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cfceb6077224d18dc0f2",
+    "url": "/static/js/main.cfceb607.chunk.js"
+  },
+  {
+    "revision": "edde28e416bd4784adfd",
+    "url": "/static/js/1.edde28e4.chunk.js"
+  },
+  {
+    "revision": "cfceb6077224d18dc0f2",
+    "url": "/static/css/main.e3218b65.chunk.css"
+  },
+  {
+    "revision": "4ec9b920b397f854a74e0b8072a9ff02",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const App = props => (
-    <div>testing</div>
-)
-
-export default App
