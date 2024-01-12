bf6e688ff596d394d79fbc960ff9d5fc95f71fef
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-01-12 16:12:59 +0700

    reactivate the project

--- src/registerFirebase.js
+++ src/registerFirebase.js
@@ -1,9 +1,7 @@
 import firebase from 'firebase/app'
 
-const isProd = () => (process.env.NODE_ENV === 'production')
-
-const apiKey = isProd() ? process.env.REACT_APP_API_KEY : process.env.REACT_APP_API_KEY_DEV
-const projectId = isProd() ? process.env.REACT_APP_PROJECT_NAME : process.env.REACT_APP_PROJECT_NAME_DEV
+const apiKey = process.env.REACT_APP_API_KEY
+const projectId = process.env.REACT_APP_PROJECT_NAME
 const authDomain = `${projectId}.firebaseapp.com`
 const databaseURL = `https://${projectId}.firebaseio.com`
 const storageBucket = `${projectId}.appspot.com`
