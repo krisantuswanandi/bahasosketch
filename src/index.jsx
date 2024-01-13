48002c005b9e7be34557d2d3cd2af77d04df709a
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-01-13 18:27:24 +0700

    remove service worker

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,4 @@ import ReactDOM from 'react-dom'
 import Root from './Root'
 import './assets/styles/style.css'
 
-import registerServiceWorker from './registerServiceWorker'
-
-registerServiceWorker()
-
 ReactDOM.render(<Root/>, document.getElementById('root'))
